d94df7e6d1524a5a43f0bdb36b15438bcae8d42b
Thomas Greif <t.greif@medat.de>
2025-03-18 14:58:44 +0100

    Set theme to catppuccin

--- init.lua
+++ init.lua
@@ -831,13 +831,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'shaunsingh/nord.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'nord'
+      vim.cmd.colorscheme 'catppuccin-macchiato'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -846,6 +846,9 @@ require('lazy').setup({
   {
     'folke/tokyonight.nvim',
     priority = 1000,
+  },
+  {
+    'DanilaMihailov/beacon.nvim',
   },
 
   -- Highlight todo, notes, etc in comments
